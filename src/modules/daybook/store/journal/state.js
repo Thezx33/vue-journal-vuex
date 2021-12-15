57c37bc0ee281320382bbeceec164423e6604799
thezx <oscarmiguelayalaruelas@gmail.com>
2021-12-14 17:52:49 -0700

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,26 @@
+
+export default () => ({
+
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'lkadsjldsakjdsakl jlkadsajlkdsajlk dsajlkdsajlkdsajlkdsadsajkl ajlskdsadsadsadsa  dsa oiewqewoijkladsjkads',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'ñlkñlk owpqeoi poqieoieqwio jkladsjlkzxc.,  n,mnm,zxcm,nsadlk oiqweoiqewoiu  sad',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'lñklkñ qweqew qewoiueqwuo asdjkhashdjz ,xczmnxnzc,m oi oiuuqew lkjadsj oidsaoiwqelkn',
+            picture: null,
+        },
+    ]
+
+})
